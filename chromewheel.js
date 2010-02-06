eb316c4d1e60cdbffd45541a891901f444a13f3e
Michael Bayne <mdb@samskivert.com>
2010-02-06 23:55:40 +0000

    A Chrome extension to perform full-page scrolling on shift-mousewheel. I'd make it do ctrl-mousewheel but that's annoyingly intercepted by the page zooming code. Maybe I need to build my own custom Chrome...

---
+++ chromewheel.js
@@ -0,0 +1,12 @@
+(function() {
+    function handleMousewheel (event) {
+        if (event.shiftKey) {
+            // Chrome flips x and y when shift is held, so we have to manually reverse
+            window.scrollBy(event.wheelDeltaY * -10, event.wheelDeltaX * -10);
+        } else if (event.ctrlKey) {
+            // alas, Chrome intercepts Ctrl-mousewheel before we ever see it
+            window.scrollBy(event.wheelDeltaX * -10, event.wheelDeltaY * -10);
+        }
+    }
+    document.addEventListener("mousewheel", handleMousewheel, false);
+})();
